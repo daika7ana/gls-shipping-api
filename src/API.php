c6c659c86ee57695ef009b0a4c1e82ce452105d2
Dan-Radu Pana <danradu.pana@gmail.com>
2019-03-06 11:47:56 +0000

    Indent fix

--- src/API.php
+++ src/API.php
@@ -3,195 +3,200 @@
 namespace GLS;
 
 use Buzz\Browser;
-use nusoap_client;
 use Buzz\Client\Curl;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use nusoap_client;
 use SimpleXMLElement;
 use Symfony\Component\DomCrawler\Crawler;
-use Doctrine\Common\Annotations\AnnotationRegistry;
 
 // Validation autoloading
 AnnotationRegistry::registerLoader(function ($name) {
-	return class_exists($name);
+    return class_exists($name);
 });
 
-class API {
-
+class API
+{
 	const STATUS_DELIVERED = '05-Delivered';
-	const STATUS_IN_DELIVERY = '04-In delivery van';
-	const STATUS_ARRIVING_TO_DEPOT = '03-Arriving in Depot';
-	const STATUS_DEPOT_TRANSIT = '53-DEPOT TRANSIT';
-	const STATUS_PICKED_UP = '86-Pacel picked up';
-	const STATUS_CLIENT_DATA_RECEIVED = '51-Client data received';
-	const STATUS_HOLD_IN_DEPOT = '07-Hold in depot';
-	const STATUS_CLIENT_NOT_AT_HOME = '12-Consignee is not at home';
+    const STATUS_IN_DELIVERY = '04-In delivery van';
+    const STATUS_ARRIVING_TO_DEPOT = '03-Arriving in Depot';
+    const STATUS_DEPOT_TRANSIT = '53-DEPOT TRANSIT';
+    const STATUS_PICKED_UP = '86-Pacel picked up';
+    const STATUS_CLIENT_DATA_RECEIVED = '51-Client data received';
+    const STATUS_HOLD_IN_DEPOT = '07-Hold in depot';
+    const STATUS_CLIENT_NOT_AT_HOME = '12-Consignee is not at home';
 
-	protected $urls = [
-		'HU' => 'http://online.gls-hungary.com/webservices/soap_server.php?wsdl',
-		'SK' => 'http://online.gls-slovakia.sk/webservices/soap_server.php?wsdl',
-		'CZ' => 'http://online.gls-czech.com/webservices/soap_server.php?wsdl',
-		'RO' => 'http://online.gls-romania.ro/webservices/soap_server.php?wsdl',
-		'SI' => 'http://connect.gls-slovenia.com/webservices/soap_server.php?wsdl',
-		'HR' => 'http://online.gls-croatia.com/webservices/soap_server.php?wsdl',
-	];
+    protected $urls = [
+        'HU' => 'http://online.gls-hungary.com/webservices/soap_server.php?wsdl',
+        'SK' => 'http://online.gls-slovakia.sk/webservices/soap_server.php?wsdl',
+        'CZ' => 'http://online.gls-czech.com/webservices/soap_server.php?wsdl',
+        'RO' => 'http://online.gls-romania.ro/webservices/soap_server.php?wsdl',
+        'SI' => 'http://connect.gls-slovenia.com/webservices/soap_server.php?wsdl',
+        'HR' => 'http://online.gls-croatia.com/webservices/soap_server.php?wsdl',
+    ];
 
-	protected $countryCode = '';
+    protected $countryCode = '';
 
-	/**
-	 * API constructor
-	 *
-	 * @param string $countryCode HU/SK/CZ/RO/SI/HR
-	 */
-	public function __construct($countryCode)
-	{
-		$this->countryCode = strtoupper($countryCode);
-	}
-
-	/**
-	 * Get parcel/s number/s
-	 *
-	 * @param Form\ParcelGeneration $form
-	 * @return array <pre> {
-	 *      tracking_code: '123', // or ['123', '124']
-	 *      raw_pdf: => 'pdfrawdatastring'
-	 * } </pre>
-	 * @throws Exception\ParcelGeneration
-	 */
-	public function generateParcel(Form\ParcelGeneration $form) 
-	{
-		$form->setPrintit(TRUE)->validate();
-
-		try {
-			$data = $this->requestNuSOAP('printlabel', $form);
-		}
-		catch (\SoapFault $e) {
-			throw new Exception\ParcelGeneration($e->getMessage());
-		}
-
-		if (empty($data['successfull'])) {
-			throw new Exception\ParcelGeneration(
-				'Response with error',
-				$data['errcode'] ?
-					"{$data['errcode']}: {$data['errdesc']}" :
-					"Unknown error - no errcode received"
-			);
-		}
-
-		if (!count($data[ 'pcls' ])) 
-			throw new Exception\ParcelGeneration("No parcels numbers received!");
-
-		return [
-			'tracking_code' => 1 == count($data[ 'pcls' ]) ? $data[ 'pcls' ][0] : $data[ 'pcls' ],
-			'raw_pdf' => !empty($data['pdfdata']) ? $data['pdfdata'] : FALSE,
-		];
-	}
-
-	/**
-	 * Regenerate PDF based on the already known PclID
-	 * 
-	 * @param String $parcel_id required for PDF generation
-	 * 
-	 * @param Array $data must contain: ['username', 'password', 'senderid', 'printertemplate']
-	 * @throws Exception
-	 * 
-	 * @return SimpleXMLElement
-	 */
-	public function getParcelPdf(String $parcel_id, Array $requested_data): SimpleXMLElement
+    /**
+     * API constructor
+     *
+     * @param string $countryCode HU/SK/CZ/RO/SI/HR
+     */
+    public function __construct($countryCode)
     {
-		$required_keys = ['username', 'password', 'senderid', 'printertemplate'];
-		$missing_keys = array_diff_key(array_flip($required_keys), $requested_data);
-
-		if($missing_keys) 
-			throw new Exception('The provided array has missing keys.');
-
-		$requested_parcel_pdf = "<?xml version='1.0' encoding = 'UTF-8'?><DTU RequestType = 'GlsApiRequest' MethodName='printLabels'><Shipments><Shipment><PclIDs><long>{$parcel_id}</long></PclIDs></Shipment></Shipments></DTU>";
-
-		$requested_data['data'] = base64_encode(gzencode($requested_parcel_pdf,9));
-			$requested_data['is_autoprint_pdfs'] = false;
-
-		$response = $this->requestNuSOAP('getprintedlabels_gzipped_xml', $requested_data);
-		$xml = gzdecode($response);
-
-		return simplexml_load_string($xml);
+        $this->countryCode = strtoupper($countryCode);
     }
 
-	/**
-	 * Get parcel status
-	 *
-	 * @param $tracking_code
-	 * @return mixed
-	 * @throws Exception
-	 */
-	public function getParcelStatus($tracking_code) 
-	{
-		$html = $this->request($this->getTrackingUrl($tracking_code));
-		$dom = new Crawler($html);
-		$row = $dom->filter('table tr.colored_0, table tr.colored_1')->first();
+    /**
+     * Get parcel/s number/s
+     *
+     * @param Form\ParcelGeneration $form
+     * @return array <pre> {
+     *      tracking_code: '123', // or ['123', '124']
+     *      raw_pdf: => 'pdfrawdatastring'
+     * } </pre>
+     * @throws Exception\ParcelGeneration
+     */
+    public function generateParcel(Form\ParcelGeneration $form)
+    {
+        $form->setPrintit(true)->validate();
 
-		if (!count($row)) 
-			throw new Exception('Tracking code wasn`t registered or error occured!');
+        try {
+            $data = $this->requestNuSOAP('printlabel', $form);
+        } catch (\SoapFault $e) {
+            throw new Exception\ParcelGeneration($e->getMessage());
+        }
 
-		$data = array_map('trim', [
-			'date' => $row->filter('td')->eq(0)->text(),
-			'status' => $row->filter('td')->eq(1)->text(),
-			'depot' => $row->filter('td')->eq(2)->text(),
-			'info' => $row->filter('td')->eq(3)->text()
-		]);
+        if (empty($data['successfull'])) {
+            throw new Exception\ParcelGeneration(
+                'Response with error',
+                $data['errcode'] ?
+                "{$data['errcode']}: {$data['errdesc']}" :
+                "Unknown error - no errcode received"
+            );
+        }
 
-		return $data['status'];
-	}
+        if (!count($data['pcls'])) {
+            throw new Exception\ParcelGeneration("No parcels numbers received!");
+        }
 
-	public function getTrackingUrl($parcelNumber, $language = 'en') 
-	{
-		return "http://online.gls-hungary.com/tt_page.php?tt_value=$parcelNumber&lng=$language";
-	}
+        return [
+            'tracking_code' => 1 == count($data['pcls']) ? $data['pcls'][0] : $data['pcls'],
+            'raw_pdf' => !empty($data['pdfdata']) ? $data['pdfdata'] : false,
+        ];
+    }
 
-	/**
-	 * @param string $method
-	 * @param array|Form $data
-	 * @throws \SoapFault
-	 * @return mixed
-	 */
-	protected function requestNuSOAP($method, $data = array()) 
-	{
-		if ($data instanceof Form) 
-			$data = $data->toArray();
+    /**
+     * Regenerate PDF based on the already known PclID
+     *
+     * @param String $parcel_id required for PDF generation
+     *
+     * @param Array $data must contain: ['username', 'password', 'senderid', 'printertemplate']
+     * @throws Exception
+     *
+     * @return SimpleXMLElement
+     */
+    public function getParcelPdf(String $parcel_id, array $requested_data): SimpleXMLElement
+    {
+        $required_keys = ['username', 'password', 'senderid', 'printertemplate'];
+        $missing_keys = array_diff_key(array_flip($required_keys), $requested_data);
 
-		$client = new nusoap_client($this->getApiUrl(), 'wsdl');
-		$client->soap_defencoding = 'UTF-8';
-		$client->decode_utf8 = false; 
+        if ($missing_keys) {
+            throw new Exception('The provided array has missing keys.');
+        }
 
-		ob_start();
-		$result = $client->call($method, $data);
-		ob_end_clean();
+        $requested_parcel_pdf = "<?xml version='1.0' encoding = 'UTF-8'?><DTU RequestType = 'GlsApiRequest' MethodName='printLabels'><Shipments><Shipment><PclIDs><long>{$parcel_id}</long></PclIDs></Shipment></Shipments></DTU>";
 
-		return $result;
-	}
+        $requested_data['data'] = base64_encode(gzencode($requested_parcel_pdf, 9));
+        $requested_data['is_autoprint_pdfs'] = false;
 
-	protected function request($url, $data = array(), $method = 'GET', array $headers = array()) 
-	{
-		if ($data instanceof Form) 
-			$data = $data->toArray();
+        $response = $this->requestNuSOAP('getprintedlabels_gzipped_xml', $requested_data);
+        $xml = gzdecode($response);
 
-		$client = new Curl();
-		$client->setVerifyPeer(FALSE);
-		
-		$browser  = new Browser($client);
-		$response = $browser->submit($url, $data, $method, $headers);
+        return simplexml_load_string($xml);
+    }
 
-		return $response->getContent();
-	}
+    /**
+     * Get parcel status
+     *
+     * @param $tracking_code
+     * @return mixed
+     * @throws Exception
+     */
+    public function getParcelStatus($tracking_code)
+    {
+        $html = $this->request($this->getTrackingUrl($tracking_code));
+        $dom = new Crawler($html);
+        $row = $dom->filter('table tr.colored_0, table tr.colored_1')->first();
 
-	/**
-	 * Get api url based on country code
-	 *
-	 * @throws Exception
-	 * @return string
-	 */
-	protected function getApiUrl()
-	{
-		if (empty($this->urls[$this->countryCode])) 
-			throw new Exception('Wrong country code - ' . $this->countryCode);
+        if (!count($row)) {
+            throw new Exception('Tracking code wasn`t registered or error occured!');
+        }
 
-		return $this->urls[$this->countryCode];
-	}
+        $data = array_map('trim', [
+            'date' => $row->filter('td')->eq(0)->text(),
+            'status' => $row->filter('td')->eq(1)->text(),
+            'depot' => $row->filter('td')->eq(2)->text(),
+            'info' => $row->filter('td')->eq(3)->text(),
+        ]);
+
+        return $data['status'];
+    }
+
+    public function getTrackingUrl($parcelNumber, $language = 'en')
+    {
+        return "http://online.gls-hungary.com/tt_page.php?tt_value=$parcelNumber&lng=$language";
+    }
+
+    /**
+     * @param string $method
+     * @param array|Form $data
+     * @throws \SoapFault
+     * @return mixed
+     */
+    protected function requestNuSOAP($method, $data = array())
+    {
+        if ($data instanceof Form) {
+            $data = $data->toArray();
+        }
+
+        $client = new nusoap_client($this->getApiUrl(), 'wsdl');
+        $client->soap_defencoding = 'UTF-8';
+        $client->decode_utf8 = false;
+
+        ob_start();
+        $result = $client->call($method, $data);
+        ob_end_clean();
+
+        return $result;
+    }
+
+    protected function request($url, $data = array(), $method = 'GET', array $headers = array())
+    {
+        if ($data instanceof Form) {
+            $data = $data->toArray();
+        }
+
+        $client = new Curl();
+        $client->setVerifyPeer(false);
+
+        $browser = new Browser($client);
+        $response = $browser->submit($url, $data, $method, $headers);
+
+        return $response->getContent();
+    }
+
+    /**
+     * Get api url based on country code
+     *
+     * @throws Exception
+     * @return string
+     */
+    protected function getApiUrl()
+    {
+        if (empty($this->urls[$this->countryCode])) {
+            throw new Exception('Wrong country code - ' . $this->countryCode);
+        }
+
+        return $this->urls[$this->countryCode];
+    }
 }
